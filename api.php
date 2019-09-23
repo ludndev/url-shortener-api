a2cb9e39fb70f69343b0d9e9a67a7c1bad31a0e1
ludndev <54596310+ludndev@users.noreply.github.com>
2019-09-23 17:56:56 +0100

    UPDATE api.php
    - Correct index.php
    - Set composer loader and base directory loader

--- api.php
+++ api.php
@@ -4,15 +4,56 @@
 namespace Ludndev\UrlShortener;
 
 
+
 /**
-* 
-*/
+ * API Class
+ *
+ * Main class of API Package
+ * 
+ * @package    Url Shortener
+ * @subpackage API
+ * @author     Ludndev < ludndev@gmail.com >
+ */
 class API 
 {
-	
-	function __construct()
+
+
+	/**
+	 * API Constructor
+	 *
+	 * @access public
+	 * @return object
+	 */
+	public function __construct()
 	{
-		# code...
+		return $this;
 	}
+
+
+	/**
+	 * Make base directory easier to resolve
+	 *
+	 * @access public
+	 * @return object
+	 */
+	public function Resolver():object
+	{
+		define( 'DIR' , __DIR__ );
+		return $this;
+	}
+
+
+	/**
+	 * Initialize composer packages
+	 *
+	 * @access public
+	 * @return object
+	 */
+	public function Composer():object
+	{
+		require( DIR . '/vendor/autoload.php' );
+		return $this;
+	}
+
 
 }
--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 
-require_once( __DIR__ . '/../api.php' );
+require_once( __DIR__ . './../api.php' );
 
 
 try {
