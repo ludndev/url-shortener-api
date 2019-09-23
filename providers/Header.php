cf8d7c2b858dc54546b5d69823bbf4be235f3331
ludndev <54596310+ludndev@users.noreply.github.com>
2019-09-23 21:16:18 +0100

    UPDATE header.php
    Remove rest methods

--- providers/Header.php
+++ providers/Header.php
@@ -15,61 +15,6 @@ class Header
 
 
 	/**
-	 * Available methods
-	 *
-	 * @access protected
-	 * @var array $allowMethods
-	 */
-	protected $allowMethods = [
-		'GET',
-		'POST',
-		'DELETE',
-		'PUT',
-		'HEAD'
-	];
-	
-
-	/**
-	 * Contruct Restfull Headers
-	 *
-	 * @access public
-	 * @param string $method
-	 * @param int $age
-	 * @todo Document link for each Header function, PhP official link
-	 * @return void
-	 */
-	public function __construct(string $method, int $age = 3600)
-	{
-		$method = strtoupper($method);
-
-		$this->CheckMethod($method);
-
-		$this->SetJson();
-		$this->SetOrigin('*');
-		$this->SetMethod($method);
-		$this->SetAge(3600);
-
-		$this->SpecialHeader($method);
-
-	}
-
-
-	/**
-	 * Allow only selected methods
-	 *
-	 * @access protected
-	 * @param string $method
-	 * @throws UNALLOWED_METHOD
-	 * @return void
-	 */
-	protected function CheckMethod(string $method):void
-	{
-		if ( in_array($this->allowMethods, $method) )
-			throw new Exception("UNALLOWED_METHOD", 1);
-	}
-
-
-	/**
 	 * Set Content-Type to JSON
 	 *
 	 * @access protected
@@ -145,31 +90,6 @@ class Header
 		if ( $status )
 			header("Access-Control-Allow-Credentials: TRUE");
 	}
-
-
-	/**
-	 * Some headers according to the current method
-	 *
-	 * @access protected
-	 * @param string $DSN
-	 * @return void
-	 */
-	protected function SpecialHeader(string $method):void
-	{
-		switch ($method) {
-			case 'POST':
-				$this->SetHeaders("Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-				break;
-
-			case 'GET':
-				$this->SetCredentials(TRUE);
-				break;
-		}
-	}
-
-
-
-	
 
 
 	/**
