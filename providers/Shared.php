db4c64612407df70a0f886e1c2acc4a583faa4a6
ludndev <54596310+ludndev@users.noreply.github.com>
2019-09-23 19:20:46 +0100

    CREATE shared.php
    - For common function, utilities
    - Always public and static

---
+++ providers/Shared.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Ludndev\UrlShortener\API\Providers;
+
+
+/**
+* 
+*/
+class Shared 
+{
+    
+    /**
+     * Function_description
+     *
+     * @access public
+     * @static
+     * @see link
+     * @param string $param
+     * @return type
+     */
+    public static function funct(string $param)
+    {
+        # code
+    }
+
+}
