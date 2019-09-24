8b1ac1e22038e8c68c29f589cde018767b735f20
ludndev <54596310+ludndev@users.noreply.github.com>
2019-09-24 01:15:07 +0100

    UPDATE shared.php
    Add autoloader for controllers

--- providers/Shared.php
+++ providers/Shared.php
@@ -9,7 +9,59 @@ namespace Ludndev\UrlShortener\API\Providers;
 */
 class Shared 
 {
-    
+
+
+    /**
+     * A proper way to scan a directory, with dots
+     *
+     * @access public
+     * @static
+     * @param string $path
+     * @return array
+     */
+    public static function ProperScandir(string $path):array
+    {
+        return 
+            array_slice(
+                scandir( $path ), 2
+            );
+    }
+
+
+
+
+    /**
+     * A proper way to scan a directory, with dots
+     *
+     * @access public
+     * @static
+     * @param string $path
+     * @return array
+     */
+    public static function ControllersLoader():array
+    {
+        $loaders = [];
+
+        $controllers = self::ProperScandir( DIR . './controllers/' );
+
+        foreach ($controllers as $controller) {
+
+            if ( is_dir( DIR . "./controllers/$controller/" ) ) {
+
+                $controller = self::ProperScandir( DIR . "./controllers/$controller/" );
+
+                foreach ($controller as $loader) {
+                    if ( preg_match('/^.*\.(loader.php)$/i', $loader) )
+                        $loaders[] = $loader;
+                }
+
+            }
+
+        }
+
+        return $loaders;
+    }
+
     /**
      * Function_description
      *
@@ -23,5 +75,6 @@ class Shared
     {
         # code
     }
+
 
 }
