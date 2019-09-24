cdb56da3cd27247e97dfa5fcef7770c6070e9f6c
ludndev <54596310+ludndev@users.noreply.github.com>
2019-09-24 05:17:38 +0100

    UPDATE router.php
    Add some rule to test inf phroute is working well

--- providers/Router.php
+++ providers/Router.php
@@ -4,15 +4,63 @@
 namespace Ludndev\UrlShortener\API\Providers;
 
 
+use \Phroute\Phroute\RouteCollector;
+use \Phroute\Phroute\Dispatcher;
+
+
 /**
 * 
 */
 class Router 
 {
+
+
+	protected $router;
+
 	
-	function __construct()
+	public function __construct()
 	{
-		# code...
+		$this->Initialize()->table();
+
+		return $this;
 	}
 
+
+	public function table()
+	{
+
+		$this->router->get('/', function(){
+		    return 'Home';
+		});
+
+		$this->router->get('/example', function(){
+		    return 'This route responds to requests with the GET method at the path /example';
+		});
+
+		return $this;
+	}
+
+
+	public function Initialize()
+	{
+		$this->router = new RouteCollector();
+		return $this;
+	}
+
+
+	public function Response()
+	{
+		$dispatcher =  new Dispatcher($this->router->getData());
+		return $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+	}
+
+
 }
+
+
+
+
+
+
+
+
