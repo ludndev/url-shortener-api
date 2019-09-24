db5035db813c9986a12de965864b678dd1793b2b
ludndev <54596310+ludndev@users.noreply.github.com>
2019-09-24 01:55:48 +0100

    UPDATE index.php
    Minimal code to run api

--- public/index.php
+++ public/index.php
@@ -1,15 +1,44 @@
 <?php
 
 
-require_once( __DIR__ . './../api.php' );
+require( __DIR__ . './../api.php' );
 
 
 try {
 
-	/* main code */
-	
+
+	$api = new Ludndev\UrlShortener\API();
+
+	/* load api */
+	$api->Loader();
+
+    /* connect to database */
+	$db = new Ludndev\UrlShortener\API\Providers\DBController(
+        $_ENV['DB_DRIVER'],
+        $_ENV['DB_HOST'],
+        $_ENV['DB_NAME'],
+        $_ENV['DB_USER'],
+        $_ENV['DB_PASS'],
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_CASE => PDO::CASE_NATURAL,
+            PDO::ATTR_ORACLE_NULLS => PDO::NULL_EMPTY_STRING
+        ]
+    );
+    
+
 } catch (Exception $error) {
 
-	/* handling exception */
+    /* catch error message or code */
+	$api->SetError( $error->getMessage() );
 	
+} finally {
+
+    /* display JSON response */
+	$api->Response();
+
 }
+
+
+
+
